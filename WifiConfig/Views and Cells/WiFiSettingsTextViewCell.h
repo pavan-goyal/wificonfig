0384ddd548af8ea6ae0d59777a1e3381b763681d
Pavan Goyal <pavangoyal.iiitm@gmail.com>
2016-05-06 22:57:19 +0530

    add `WiFiSettingsTextViewCell`

---
+++ WifiConfig/Views and Cells/WiFiSettingsTextViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WiFiSettingsTextViewCell.h
+//  WifiConfig
+//
+//  Created by Pavan Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WiFiSettingsTextViewCell : UITableViewCell
+
+@end
---
+++ WifiConfig/Views and Cells/WiFiSettingsTextViewCell.m
@@ -0,0 +1,24 @@
+//
+//  WiFiSettingsTextViewCell.m
+//  WifiConfig
+//
+//  Created by Pavan Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import "WiFiSettingsTextViewCell.h"
+
+@implementation WiFiSettingsTextViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
