b241d98c6d7bfca6502d554d4a234f2581ca6a1b
Pavan Goyal <pavangoyal.iiitm@gmail.com>
2016-05-06 22:41:28 +0530

    add `WifiConfigModel.h` and `WifiConfigModel.m`

---
+++ WifiConfig/Models/WifiConfigModel.h
@@ -0,0 +1,13 @@
+//
+//  WifiConfigModel.h
+//  WifiConfig
+//
+//  Created by Pavan Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WifiConfigModel : NSObject
+
+@end
