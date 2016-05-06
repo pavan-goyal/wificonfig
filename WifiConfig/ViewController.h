c0f938691aaeabe8a1801597da6b9ad34068a9ec
Pavan Goyal <pavan.goyal@timesinternet.in>
2016-05-06 22:06:23 +0530

    Initial Commit

---
+++ WifiConfig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WifiConfig
+//
+//  Created by Pavan Kumar Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WifiConfig/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WifiConfig
+//
+//  Created by Pavan Kumar Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
