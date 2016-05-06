c550dd592b384d0883eee183731b1ca83fbc37cf
Pavan Goyal <pavangoyal.iiitm@gmail.com>
2016-05-06 23:05:28 +0530

    add `WiFiSettingsHeaderView.xib, WiFiSettingsHeaderView.h and WiFiSettingsHeaderView.m`

---
+++ WifiConfig/Views and Cells/WiFiSettingsHeaderView.m
@@ -0,0 +1,14 @@
+//
+//  WiFiSettingsHeaderView.m
+//  WifiConfig
+//
+//  Created by Pavan Goyal on 5/6/16.
+//  Copyright © 2016 Pavan Goyal. All rights reserved.
+//
+
+#import "WiFiSettingsHeaderView.h"
+
+@implementation WiFiSettingsHeaderView
+
+
+@end
